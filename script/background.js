eb91ddcfc4d125eb27e8ed9ef70e5b3eaa59b52e
kubajss <77906224+kubajss@users.noreply.github.com>
2021-07-15 16:05:00 +0200

    Update background.js

--- script/background.js
+++ script/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, res) {
         chrome.downloads.download({
             url: request.download,
-            filename: request.download.replace("https://tmnforever.tm-exchange.com/get.aspx?action=recordgbx&id=", "") + '.Replay.Gbx'
+            filename: request.download.replace("https://tmnforever.tm-exchange.com/get.aspx?action=trackgbx&id=", "") + '.Challenge.Gbx'
         });
     }
 )
